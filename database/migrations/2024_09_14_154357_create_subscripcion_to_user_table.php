b56fd5ff006e9ed48a70de2d0712dda58757ac18
ChurroDevelop <jhonfredy2325@gmail.com>
2024-09-14 11:06:46 -0500

    Agregando migraciones

---
+++ database/migrations/2024_09_14_154357_create_subscripcion_to_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscripcion_to_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('documento')->on('users')->nullOnDelete();
+            $table->unsignedBigInteger('subscripcion_id')->nullable();
+            $table->foreign('subscripcion_id')->references('id')->on('subscripciones')->nullOnDelete(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscripcion_to_user');
+    }
+};
---
+++ database/migrations/2024_09_14_155425_create_evaluaciones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evaluaciones', function (Blueprint $table) {
+            $table->id();
+            $table->double('nota');
+            $table->boolean('estado');
+            $table->bigInteger('doc_usuario');
+            $table->unsignedBigInteger('curso_id')->nullable();
+            $table->foreign('curso_id')->references('id')->on('cursos')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evaulaciones');
+    }
+};
